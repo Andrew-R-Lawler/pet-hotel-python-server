bbdc03b45817aeee63c6138dbf333ec5b4d50c3b
Andrew Lawler <Andrew.R.Lawler@gmail.com>
2020-07-16 15:01:37 -0500

    /owner post functions

--- server.py
+++ server.py
@@ -1,5 +1,5 @@
 import requests
-from flask import Flask, jsonify, json
+from flask import Flask, jsonify, json, request
 from flask_cors import CORS
 import psycopg2
 
@@ -19,8 +19,15 @@ def hello_world():
     cur = con.cursor()
     cur.execute("select * from owners")
     rows = cur.fetchall()
-    owners=[]
-    owner={}
-    for r in rows:
-        
     return jsonify(rows)
+
+@app.route('/owner', methods=['POST'])
+def post():
+    cur=con.cursor()
+    owner = request.get_json()
+    first_name = owner["firstName"]
+    last_name = owner["lastName"]
+    cur.execute("insert into owners (first_name, last_name) values (%s, %s);", (first_name, last_name))
+    con.commit()
+    cur.close()
+    return 'Hello'
